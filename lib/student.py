4fad02c0335348757bb08764c46d938ead5272c3
gideonkkk <gideon.kariuki@student.moringaschool.com>
2024-09-16 11:32:55 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+# lib/student.py
 
-from user import User
+from user import User  # Ensure this import exists
 
-class Student(User):
-    
-    def learn(self):
-        pass
+class Student(User):  # Inheriting from User
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Initialize with an empty list
+
+    def learn(self, knowledge):
+        self.knowledge.append(knowledge)
